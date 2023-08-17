60a94756f4f79c048beefc9ec24193726c81b2b4
Andrii Chernyshov <andruha46@gmail.com>
2023-08-17 11:57:28 +0300

    created main_activity.xml created MainActivity.kt add to manifest main activity

---
+++ app/src/main/java/com/example/rdhomework/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rdhomework
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+    }
+}
