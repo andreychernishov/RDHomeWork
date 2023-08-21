3bc093d677d5520680fda20dd4ce87d89fa95198
Andrii Chernyshov <andruha46@gmail.com>
2023-08-21 15:11:19 +0300

    created main_activity.xml created MainActivity.kt add to manifest main activity homework done

--- app/src/main/java/com/example/rdhomework/MainActivity.kt
+++ app/src/main/java/com/example/rdhomework/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.rdhomework
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        val phoneTextView : TextView = findViewById(R.id.phone_tv)
+        val emailTextView : TextView = findViewById(R.id.email_tv)
+        phoneTextView.setOnClickListener {
+            Toast.makeText(this, "Phone TV clicked", Toast.LENGTH_SHORT).show()
+        }
+        emailTextView.setOnClickListener {
+            Toast.makeText(this, "Email TV clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
